b4b1a46736c66a3ad3c2e276eb72994ac1da42e8
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-12-10 15:19:02 +0900

    bugfix

--- resources/js/Pages/Journals/Index.vue
+++ resources/js/Pages/Journals/Index.vue
@@ -66,28 +66,37 @@ function saveToDatabase() {
         </Transition>
 
         <div class="mx-auto max-w-2xl p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="saveToDatabase()">
-                <input v-model="form.date" type="date" name="date" id="date" />
-                <input
-                    v-model="form.height"
-                    type="number"
-                    name="height"
-                    id="height"
-                />
-                <input
-                    v-model="form.weight"
-                    type="number"
-                    name="weight"
-                    id="weight"
-                />
-                <textarea
-                    v-model="form.entry"
-                    placeholder="What did your child do today?"
-                    class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                ></textarea>
-                <InputError :message="form.errors.entry" class="mt-2" />
-                <PrimaryButton class="mt-4 bg-teal-500">Submit</PrimaryButton>
-            </form>
+            <div>
+                <form @submit.prevent="saveToDatabase()">
+                    <input
+                        v-model="form.date"
+                        type="date"
+                        name="date"
+                        id="date"
+                    />
+                    <input
+                        v-model="form.height"
+                        type="number"
+                        name="height"
+                        id="height"
+                    />
+                    <input
+                        v-model="form.weight"
+                        type="number"
+                        name="weight"
+                        id="weight"
+                    />
+                    <textarea
+                        v-model="form.entry"
+                        placeholder="What did your child do today?"
+                        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    ></textarea>
+                    <InputError :message="form.errors.entry" class="mt-2" />
+                    <PrimaryButton class="mt-4 bg-teal-500"
+                        >Submit</PrimaryButton
+                    >
+                </form>
+            </div>
 
             <div class="mt-6 divide-y rounded-lg bg-white shadow-sm">
                 <Journal

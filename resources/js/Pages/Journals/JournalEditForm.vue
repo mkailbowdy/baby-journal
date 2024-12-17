07292403edbe8abfc2d97a9451ca2cc13aaf6537
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-12-17 18:25:57 +0900

    refactored successfully

---
+++ resources/js/Pages/Journals/JournalEditForm.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { Journal } from '@/types/Journal';
+import { useForm } from '@inertiajs/vue3';
+const emit = defineEmits(['journalUpdated', 'journalDeleted']);
+const props = defineProps(['journal']);
+
+const form = useForm({
+    entry: props.journal.entry,
+    date: props.journal.date,
+    height: props.journal.height,
+    weight: props.journal.weight,
+});
+function updateJournal(journal: Journal) {
+    form.put(route('journals.update', journal.id), {
+        preserveScroll: true,
+        onSuccess: () => {
+            emit('journalUpdated', journal);
+        },
+    });
+}
+function deleteJournal(journal: Journal) {
+    // Inertia's form helpers refreshes the props
+    form.delete(route('journals.destroy', journal.id), {
+        onSuccess: () => {
+            form.reset();
+            emit('journalDeleted');
+        },
+    });
+}
+</script>
+<template>
+    <form @submit.prevent="updateJournal(props.journal)">
+        <div class="mb-4">
+            <img alt="profile picture" :src="`storage/${journal.image}`" />
+        </div>
+        <input v-model="form.date" type="date" />
+        <input v-model="form.height" type="number" />
+        <input v-model="form.weight" type="number" />
+        <textarea
+            v-model="form.entry"
+            class="mt-4 w-full rounded-md border-gray-300 text-gray-900 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        ></textarea>
+        <InputError :message="form.errors.entry" class="mt-2" />
+        <div class="mt-4 flex justify-end gap-2">
+            <PrimaryButton
+                class="bg-red-500 hover:bg-red-700"
+                @click.prevent="deleteJournal(journal)"
+            >
+                Delete
+            </PrimaryButton>
+            <PrimaryButton
+                class="bg-gray-400 hover:bg-gray-500"
+                @click.prevent="
+                    form.reset();
+                    form.clearErrors();
+                "
+            >
+                Cancel
+            </PrimaryButton>
+            <PrimaryButton class="bg-gray-800 hover:bg-gray-700"
+                >Save</PrimaryButton
+            >
+        </div>
+    </form>
+</template>
--- resources/js/Pages/Journals/Show.vue
+++ resources/js/Pages/Journals/Show.vue
@@ -1,45 +1,24 @@
 <script setup lang="ts">
 import Dropdown from '@/Components/Dropdown.vue';
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
+import JournalEditForm from '@/Pages/Journals/JournalEditForm.vue';
 import { Journal } from '@/types/Journal';
-import { useForm } from '@inertiajs/vue3';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { ref } from 'vue';
 
 const editing = ref(false);
+const props = defineProps(['journal']);
 const emit = defineEmits(['journalUpdated', 'journalDeleted']);
 
-const props = defineProps(['journal']);
-const form = useForm({
-    entry: props.journal.entry,
-    date: props.journal.date,
-    height: props.journal.height,
-    weight: props.journal.weight,
-});
-
-function deleteJournal(journal: Journal) {
-    // Inertia's form helpers refreshes the props
-    form.delete(route('journals.destroy', journal.id), {
-        onSuccess: () => {
-            form.reset();
-            emit('journalDeleted');
-        },
-    });
-}
-
-function updateJournal(journal: Journal) {
-    form.put(route('journals.update', journal.id), {
-        preserveScroll: true,
-        onSuccess: () => {
-            editing.value = false;
-            emit('journalUpdated', journal);
-        },
-    });
-}
-
 dayjs.extend(relativeTime);
+function updateJournal(journal: Journal) {
+    editing.value = false;
+    emit('journalUpdated', journal);
+}
+function deleteJournal() {
+    editing.value = false;
+    emit('journalDeleted');
+}
 </script>
 
 <template>
@@ -103,43 +82,11 @@ dayjs.extend(relativeTime);
                 <p class="mt-4 text-lg text-gray-900">{{ journal.entry }}</p>
             </div>
             <div v-else>
-                <form @submit.prevent="updateJournal(props.journal)">
-                    <div class="mb-4">
-                        <img
-                            alt="profile picture"
-                            :src="`storage/${journal.image}`"
-                        />
-                    </div>
-                    <input v-model="form.date" type="date" />
-                    <input v-model="form.height" type="number" />
-                    <input v-model="form.weight" type="number" />
-                    <textarea
-                        v-model="form.entry"
-                        class="mt-4 w-full rounded-md border-gray-300 text-gray-900 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    ></textarea>
-                    <InputError :message="form.errors.entry" class="mt-2" />
-                    <div class="mt-4 flex justify-end gap-2">
-                        <PrimaryButton
-                            class="bg-red-500 hover:bg-red-700"
-                            @click.prevent="deleteJournal(journal)"
-                        >
-                            Delete
-                        </PrimaryButton>
-                        <PrimaryButton
-                            class="bg-gray-400 hover:bg-gray-500"
-                            @click.prevent="
-                                editing = false;
-                                form.reset();
-                                form.clearErrors();
-                            "
-                        >
-                            Cancel
-                        </PrimaryButton>
-                        <PrimaryButton class="bg-gray-800 hover:bg-gray-700"
-                            >Save</PrimaryButton
-                        >
-                    </div>
-                </form>
+                <JournalEditForm
+                    :journal="props.journal"
+                    @journal-updated="updateJournal"
+                    @journal-deleted="deleteJournal"
+                />
             </div>
         </div>
     </div>
